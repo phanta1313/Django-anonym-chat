32edd864b826e4d40e2bf04b0b7ebaf69f014fb3
root <root@ilyasbruh.asia-east2-c.c.notional-device-431910-r5.internal>
2024-09-10 08:12:07 +0000

    ws connection error

--- config/settings.py
+++ config/settings.py
@@ -114,12 +114,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static/',
-]
+STATIC_ROOT = BASE_DIR / 'static/'
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
