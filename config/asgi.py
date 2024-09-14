a6cb4c7031a7d4ec05a45e387423f206f2f5baf8
root <root@ilyasbruh.asia-east2-c.c.notional-device-431910-r5.internal>
2024-09-14 15:05:50 +0000

    server bug fix (didnt use daphne)

--- config/asgi.py
+++ config/asgi.py
@@ -1,12 +1,15 @@
 import os
 from django.core.asgi import get_asgi_application
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
 from channels.routing import ProtocolTypeRouter
 from channels.auth import AuthMiddlewareStack
 from channels.routing import URLRouter
 from web.routing import ws_urlpatterns
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
